fa6ad337fbf5238c66ddff59aff5d8b92c034483
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-07-31 15:51:41 +0300

    Add API Provider

--- travelopedia/src/components/DestinationIndex.jsx
+++ travelopedia/src/components/DestinationIndex.jsx
@@ -1,10 +1,13 @@
 import AddDestination from "./AddDestination";
+import DestinationList from "./DestinationList";
+
 
 function DestinationIndex() {
   return (
     <div className="py-4">
       <h1 className="text-success text-center">Travel List</h1>
       <AddDestination />
+      <DestinationList />
     </div>
   );
 }
---
+++ travelopedia/src/components/DestinationList.jsx
@@ -0,0 +1,5 @@
+function DestinationList() {
+  return <p>Hello</p>;
+}
+
+export default DestinationList;
