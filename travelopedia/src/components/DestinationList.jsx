bd559ee9970717a0ff90ea6c8a1797e0af1b4673
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-07-31 15:59:24 +0300

    Get Endpoint in Action

--- travelopedia/src/components/DestinationList.jsx
+++ travelopedia/src/components/DestinationList.jsx
@@ -1,5 +1,27 @@
+import { useGetAllDestinationQuery } from "../api/destinationApi";
+
 function DestinationList() {
-  return <p>Hello</p>;
+  const { data, isLoading, isSuccess, isError, error } =
+    useGetAllDestinationQuery();
+
+  let content;
+  if (isLoading) {
+    content = <p className="text-center text-info p-2">Loading...</p>;
+  } else if (isSuccess) {
+    content = data.map((destination) => {
+      return (
+        <article key={destination.id}>
+          <div className="text-center text-info p-2">
+            {destination.city}, {destination.country} - {destination.daysNeeded}{" "}
+            days
+          </div>
+        </article>
+      );
+    });
+  } else if (isError) {
+    content = <p>{error}</p>;
+  }
+  return <div>{content}</div>;
 }
 
 export default DestinationList;
