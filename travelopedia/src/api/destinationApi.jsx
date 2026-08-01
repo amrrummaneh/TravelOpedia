efb7301f150fcda3314fe9de4a4c254f4a4230dd
Amr Rummaneh <amr.rummaneh2@gmail.com>
2026-08-01 10:27:32 +0300

    Tags

--- travelopedia/src/api/destinationApi.jsx
+++ travelopedia/src/api/destinationApi.jsx
@@ -3,18 +3,25 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const destinationAPI = createApi({
   reducerPath: "apidestination",
   baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/" }),
+  tagTypes: ["Destinations"],
   endpoints: (builder) => ({
     //QUERY -> GET
     //MUTATION -> POST,PUT,DELETE
     getAllDestination: builder.query({
       query: () => "destination",
+      providesTags: ["Destinations"],
     }),
+    // getAllDestinationById: builder.query({
+    //   query: (id) => `destination/${id}`,
+    //   providesTags:(result,error,id)=> [{type:'Destination',id}]
+    // }),
     addDestination: builder.mutation({
       query: (destination) => ({
         url: "destination",
         method: "POST",
         body: destination,
       }),
+      invalidatesTags: ["Destinations"],
     }),
     updateDestination: builder.mutation({
       query: (destination) => ({
@@ -22,12 +29,15 @@ export const destinationAPI = createApi({
         method: "PUT",
         body: destination,
       }),
+      invalidatesTags: ["Destinations"],
     }),
     deleteDestination: builder.mutation({
       query: ({ id }) => ({
         url: `destination/${id}`,
         method: "DELETE",
       }),
+      invalidatesTags: ["Destinations"],
+      //invalidatesTags:(result,error,id)=> [{type:'Destination',id}]
     }),
   }),
 });
